b422ba7e21d7b4a9182e96e77ed6cd3c5a90390b
StephenSachson <msx7000@gmail.com>
2014-08-01 13:51:47 +0800

    removing redundant comments

--- Chase/Classes/PlayScene.h
+++ Chase/Classes/PlayScene.h
@@ -4,27 +4,13 @@
 //
 //  Created by deepfocus-004 on 8/1/14.
 //  Copyright deepfocus-004 2014. All rights reserved.
-//
-// -----------------------------------------------------------------------
 
-// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using cocos2d-v3
 #import "cocos2d.h"
 #import "cocos2d-ui.h"
 
-// -----------------------------------------------------------------------
-
-/**
- *  The intro scene
- *  Note, that scenes should now be based on CCScene, and not CCLayer, as previous versions
- *  Main usage for CCLayer now, is to make colored backgrounds (rectangles)
- *
- */
 @interface PlayScene : CCScene
-
-// -----------------------------------------------------------------------
 
 + (PlayScene *)scene;
 - (id)init;
 
-// -----------------------------------------------------------------------
 @end
--- Chase/Classes/PlayScene.m
+++ Chase/Classes/PlayScene.m
@@ -4,38 +4,26 @@
 //
 //  Created by deepfocus-004 on 8/1/14.
 //  Copyright deepfocus-004 2014. All rights reserved.
-//
-// -----------------------------------------------------------------------
 
-// Import the interfaces
+
 #import "PlayScene.h"
-
-// -----------------------------------------------------------------------
-#pragma mark - IntroScene
-// -----------------------------------------------------------------------
 
 @implementation PlayScene
 
-// -----------------------------------------------------------------------
 #pragma mark - Create & Destroy
-// -----------------------------------------------------------------------
 
 + (PlayScene *)scene
 {
 	return [[self alloc] init];
 }
 
-// -----------------------------------------------------------------------
-
 - (id)init
 {
-    // Apple recommend assigning self with supers return value
     self = [super init];
     if (!self) return(nil);
     
-    // Create a colored background (Dark Grey)
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-    // done
+    
 	return self;
 }
 
