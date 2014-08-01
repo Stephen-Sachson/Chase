60c5fd397af0ab7c320f5163562799b495f29aec
StephenSachson <msx7000@gmail.com>
2014-08-01 11:55:20 +0800

    init project

---
+++ Chase/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Chase
+//
+//  Created by deepfocus-004 on 8/1/14.
+//  Copyright deepfocus-004 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
